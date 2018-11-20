51a642c07d34aa4f3b612c5d1e788fe2d5a79c75
CavHaunch <cav.haunch@gmail.com>
2018-11-20 23:24:58 +0000

    basic_main added

--- main_basic.py
+++ main_basic.py
@@ -10,18 +10,7 @@ def do_something(sc,I):
     I+=1
     s.enter(1, 1, do_something, (sc,I))
 
-
-x = np.zeros(100)
-y = np.zeros(100)
 i=0
-print(i)
-fig, ax = plt.subplots()
-# red = np.linspace(0,2,100)
-# ax.plot(red,red)
-# plt.show()
 
-# I'd like to pass red and ax into do_something and
-# change red to np.linspace(0,2,100) then plot it 
-# on ax
 s.enter(1, 1, do_something, (s,i))
 s.run()
