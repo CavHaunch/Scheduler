5f1f785a731d12be4dd3f8b19ea7512d711b67f5
CavHaunch <cav.haunch@gmail.com>
2018-11-20 23:27:47 +0000

    main formatted

--- main.py
+++ main.py
@@ -5,23 +5,19 @@ import sched, time
 
 s = sched.scheduler(time.time, time.sleep)
 
-def do_something(sc,I): 
-    print(I)
-    I+=1
-    s.enter(1, 1, do_something, (sc,I))
+def do_something(sc,X,Y,I,axes): 
+    X = np.linspace(0,2,100)
+    Y = np.linspace(0,2,100)+I
+    I = I+1.0
+    axes.plot(X,np.sin(Y))
+    plt.show()
+    s.enter(1, 1, do_something, (sc,X,Y,I,axes))
 
 
 x = np.zeros(100)
 y = np.zeros(100)
-i=0
-print(i)
+i=0.0
 fig, ax = plt.subplots()
-# red = np.linspace(0,2,100)
-# ax.plot(red,red)
-# plt.show()
 
-# I'd like to pass red and ax into do_something and
-# change red to np.linspace(0,2,100) then plot it 
-# on ax
-s.enter(1, 1, do_something, (s,i))
+s.enter(1, 1, do_something, (s,x,y,i,ax))
 s.run()
