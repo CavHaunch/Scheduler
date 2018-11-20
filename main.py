0504a06d92c86f7d7ba3ff96227b38aed4fb5678
CavHaunch <cav.haunch@gmail.com>
2018-11-20 22:51:07 +0000

    Set task for Naim

--- main.py
+++ main.py
@@ -11,46 +11,9 @@ def do_something(sc):
     s.enter(1, 1, do_something, (sc,))
 
 
-#Output Image Path
-Image_Filename = "numpy/Sines/test.png"
-
-#Plot parameters 
-T_Min = 0
-T_Max = 4*3.141159*2.0/3.0
-Fs = 10
-
-#Signal Lengths
-Input_Length = int((T_Max-T_Min)*Fs)
-
-#Initialize data structures
-Index_Vector = np.zeros(Input_Length)
-r = np.zeros(Input_Length)
-g = np.zeros(Input_Length)
-b = np.zeros(Input_Length)
-P = np.zeros(Input_Length)
-
-# Data for plotting
-t = np.linspace( T_Min, T_Max, Input_Length )
-
-r = 0.5 + (0.5 * np.sin(t) )
-g =  0.5 + (0.5 *np.sin(t+(2*3.141/3)))
-b =  0.5 + (0.5 *np.sin(t-(2*3.141/3)))
-P = r**2 + g**2 + b**2
-
+red = np.linspace(0,2,100)
 fig, ax = plt.subplots()
-ax.plot(t,r)
-ax.plot(t,g)
-ax.plot(t,b)
-ax.plot(t,P)
-ax.plot()
 
-
-ax.set(xlabel='time (t)', ylabel='voltage (mV)',
-       title='About as simple as it gets, folks')
-ax.grid(True)
-
-# fig.tight_layout()
-# fig.savefig(Image_Filename)
+#I'd like to pass red and ax into do_something
 s.enter(60, 1, do_something, (s,))
 s.run()
-plt.show()
