663aaa65b75a8dacbc3592e90125c519b63803c8
CavHaunch <cav.haunch@gmail.com>
2018-11-20 22:51:46 +0000

    Ready to pass data into sched

--- main.py
+++ main.py
@@ -11,9 +11,11 @@ def do_something(sc):
     s.enter(1, 1, do_something, (sc,))
 
 
-red = np.linspace(0,2,100)
+red = np.zeros(100)
 fig, ax = plt.subplots()
 
-#I'd like to pass red and ax into do_something
+# I'd like to pass red and ax into do_something and
+# change red to np.linspace(0,2,100) then plot it 
+# on ax
 s.enter(60, 1, do_something, (s,))
 s.run()
